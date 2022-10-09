5ff97d6bcf5d959c6eaf92d77c09fc3337e0c9b4
aldoKelvianto <aldo.wachyudi@gmail.com>
2022-10-09 21:10:46 +0700

    Use version catalog for getting plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,10 @@ buildscript {
         set("compose_ui_version", "1.2.1")
     }
 }
+
 plugins {
-    id("com.android.application") version "7.3.0" apply false
-    id("com.android.library") version "7.3.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
-    id("org.jetbrains.kotlin.jvm") version "1.7.10" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
 }
