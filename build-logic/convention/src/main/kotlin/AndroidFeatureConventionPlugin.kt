22a37eb52fde44b977a9063e07910c1b3c20d01d
aldoKelvianto <aldo.wachyudi@gmail.com>
2022-10-09 17:06:51 +0700

    Remove build.src

--- build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -1,8 +1,6 @@
-import com.android.build.gradle.LibraryExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 
@@ -12,12 +10,6 @@ class AndroidFeatureConventionPlugin : Plugin<Project> {
             pluginManager.apply {
                 apply("nowinandroid.android.library")
                 apply("nowinandroid.android.hilt")
-            }
-            extensions.configure<LibraryExtension> {
-                defaultConfig {
-                    testInstrumentationRunner =
-                        "com.google.samples.apps.nowinandroid.core.testing.NiaTestRunner"
-                }
             }
 
             val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
--- build-logic/convention/src/main/kotlin/com/example/toysapp/buildlogic/convention/AndroidCompose.kt
+++ build-logic/convention/src/main/kotlin/com/example/toysapp/buildlogic/convention/AndroidCompose.kt
@@ -1,34 +1,7 @@
 package com.example.toysapp.buildlogic.convention
 
-import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.kotlin.dsl.getByType
 import java.io.File
-
-/**
- * Configure Compose-specific options
- */
-internal fun Project.configureAndroidCompose(
-    commonExtension: CommonExtension<*, *, *, *>,
-) {
-    val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
-
-    commonExtension.apply {
-        buildFeatures {
-            compose = true
-        }
-
-        composeOptions {
-            kotlinCompilerExtensionVersion =
-                libs.findVersion("androidxComposeCompiler").get().toString()
-        }
-
-        kotlinOptions {
-            freeCompilerArgs = freeCompilerArgs + buildComposeMetricsParameters()
-        }
-    }
-}
 
 private fun Project.buildComposeMetricsParameters(): List<String> {
     val metricParameters = mutableListOf<String>()
