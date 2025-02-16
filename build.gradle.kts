3ce8c7172bd12dc91bdaee52648a41983fdd58d4
aldoKelvianto <aldo.wachyudi@gmail.com>
2025-02-16 18:34:24 +0700

    Migrate to settings.gradle and plugin from dependency-analysis to build-health

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,4 @@
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.android.library) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.jvm) apply false
-    alias(libs.plugins.dependency.analysis)
     alias(libs.plugins.module.graph.assertion) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.binary.compatibility.validator)
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,19 @@ pluginManagement {
         mavenCentral()
     }
 }
+
+plugins {
+    // From: https://docs.gradle.org/current/userguide/version_catalogs.html
+    // You cannot use a plugin declared in a version catalog in your settings file or settings plugin.
+    // alias(libs.plugins.build.health)
+    id("com.autonomousapps.build-health") version "2.8.2"
+
+    // From: https://github.com/autonomousapps/dependency-analysis-gradle-plugin/wiki/Adding-to-your-project
+    id("org.jetbrains.kotlin.jvm") version "2.1.10" apply false
+    id("com.android.application") version "8.8.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.1.10" apply false
+}
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
